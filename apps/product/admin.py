233a558eff45d7ee5307a4e5064ff18c370d9617
nurbol0tt <fairtail764@gmail.com>
2023-08-09 01:58:52 +0600

    inlcude tables

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.product.models import Product, Category, Tag
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Product)
