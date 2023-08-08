a57d9f02e8f7d58ce525f1ec8fd1bee290b916af
nurbol0tt <fairtail764@gmail.com>
2023-08-08 22:05:41 +0600

    create table

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from apps.user.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True, blank=True)
+    email = models.EmailField(blank=True, null=True, unique=True, )
+    password = models.CharField(max_length=128)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.username
